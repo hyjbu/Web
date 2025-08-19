a137818a0bff28dffd52d14a9c5be594e265fb88
HuyLam <lamhuynguyentrung2801@gmail.com>
2025-08-19 15:36:01 +0700

    Fixed scroll in position

--- src/page/position/position.js
+++ src/page/position/position.js
@@ -2,15 +2,17 @@ import './position.css'
 
 function Position() {
   return (
-    <div className="position_container">
-      <span className="posStatic">Static</span>
-      <span className="posSticky">Sticky</span>
-      <div className="posRelative">
-        Relative
-        <span className="posAbsolute">Absolute</span>
+    <div className='poscontainer'>
+      <div className="position_container">
+        <span className="posStatic">Static</span>
+        <span className="posSticky">Sticky</span>
+        <div className="posRelative">
+          Relative
+          <span className="posAbsolute">Absolute</span>
+        </div>
+  
+        <span className="posFixed">Fixed</span>
       </div>
-
-      <span className="posFixed">Fixed</span>
     </div>
   );
 }
